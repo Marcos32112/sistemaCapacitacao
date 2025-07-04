79cee0af1169186901983d497b49accc69167707
Marcos32112 <marcosantonio344r@gmail.com>
2025-07-04 11:34:25 -0300

    Aa

---
+++ sistemaCapacitacao-main(2)/sistemaCapacitacao-main/src/aplicacao/Programa.java
@@ -0,0 +1,35 @@
+package aplicacao;
+
+import entidades.Curso;
+import entidades.Product;
+import entidades.ServidorPublico;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Programa {
+
+
+
+
+
+//        excluirServidorPublico( int matricula);
+//        calcularFolhaServidoresPublicos();
+//
+        public static void main(String[] args) {
+            ServidorPublico servidorPublico  = new ServidorPublico();
+
+            servidorPublico.adicionarServidorPublico();
+            servidorPublico.adicionarServidorPublico();
+            servidorPublico.listarServidores();
+
+            servidorPublico.listarServidores(Integer.parseInt(JOptionPane.showInputDialog(null, "Informe a matricula que deseja pesquisar")));servidorPublico.listarServidores(Integer.parseInt(JOptionPane.showInputDialog(null, "Informe a matricula que deseja pesquisar")));
+
+            servidorPublico.listarServidores(Integer.parseInt(JOptionPane.showInputDialog(null, "Informe a matricula a ser excluida")));servidorPublico.listarServidores(Integer.parseInt(JOptionPane.showInputDialog(null, "Informe a matricula a ser ecluida")));
+
+    }
+
+
+}
